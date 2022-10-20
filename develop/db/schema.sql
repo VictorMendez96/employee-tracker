779a013a0d97258659a5db73a5e81a9948ee9431
V Mendez <victormendez990@gmail.com>
2022-10-19 19:35:38 -0600

    CREATE department and role tables

--- develop/db/schema.sql
+++ develop/db/schema.sql
@@ -7,15 +7,19 @@ USE employee_db;
 
 -- Create department table 
 CREATE TABLE department (
-
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(30) NOT NULL
 );
 
 -- Create role table
 CREATE TABLE role (
-
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INT
 );
 
 -- Create employee table
 CREATE TABLE employee (
-
+    
 );
