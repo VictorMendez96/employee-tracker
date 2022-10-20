a42e0b5066004c7a23eb9efa733a9e00a93d0c1d
V Mendez <victormendez990@gmail.com>
2022-10-19 19:38:51 -0600

    CREATE employee table

--- develop/db/schema.sql
+++ develop/db/schema.sql
@@ -21,5 +21,9 @@ CREATE TABLE role (
 
 -- Create employee table
 CREATE TABLE employee (
-    
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL, 
+    role_id INT, 
+    manager_id INT
 );
