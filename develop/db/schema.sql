95f23cd3fa15b2fc34c09210d38ce142d88a16e8
V Mendez <victormendez990@gmail.com>
2022-10-19 19:08:05 -0600

    Start Tables

--- develop/db/schema.sql
+++ develop/db/schema.sql
@@ -3,12 +3,19 @@ DROP DATABASE IF EXISTS employee_db;
 CREATE DATABASE employee_db; 
 
 -- Sets default database
-USE employee_db
+USE employee_db;
 
 -- Create department table 
+CREATE TABLE department (
 
+);
 
 -- Create role table
+CREATE TABLE role (
 
+);
 
 -- Create employee table
+CREATE TABLE employee (
+
+);
