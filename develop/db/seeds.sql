9be93591492f28248fd5c437d10635d8781899aa
V Mendez <victormendez990@gmail.com>
2022-10-19 20:16:55 -0600

    Add to seeds

--- develop/db/seeds.sql
+++ develop/db/seeds.sql
@@ -1,5 +1,14 @@
+-- Tell sql where the seeds go
+USE employee_db
+
 -- INSERT seeds into department
+INSERT INTO 
+VALUES
 
 -- INSERT seeds into role 
+INSERT INTO 
+VALUES
 
 -- INSERT seeds into employee 
+INSERT INTO 
+VALUES
